0e106a896ef848d09f93ae8c7dbefbab205af10d
Chris Esterbrook <cebrook290@gmail.com>
2023-04-23 00:01:56 -0700

    quick edits

--- final/src/Components/RequestForm.js
+++ final/src/Components/RequestForm.js
@@ -12,8 +12,11 @@ export default function RequestForm() {
             <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
         </div>
         <div className="mb-3">
-            <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+            <label for="exampleFormControlTextarea1" className="form-label">Movies you would like to see added or any additional comments/feedback!</label>
             <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+        <div className="col-12">
+        <button className="btn btn-primary" type="submit">Submit form</button>
         </div>
             
     </div>
